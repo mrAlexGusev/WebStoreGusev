88f6b4ef33f6a8db1f0e3550c8d21d52e429ff6b
Александр Гусев <scorp.predateur@gmail.com>
2020-07-09 11:59:55 +0300

    Добавил action методы и представления оставшихся страниц макета

--- WebStoreGusev/Controllers/HomeController.cs
+++ WebStoreGusev/Controllers/HomeController.cs
@@ -17,5 +17,45 @@ namespace WebStoreGusev.Controllers
         {
             return View();
         }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
     }
 }
