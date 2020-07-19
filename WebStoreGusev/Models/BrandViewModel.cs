7b5b78d46d151daf7411a7a47c5c7f920665797a
Александр Гусев <scorp.predateur@gmail.com>
2020-07-19 19:33:57 +0300

    ViewComponent для брендов товаров

---
+++ WebStoreGusev/Models/BrandViewModel.cs
@@ -0,0 +1,19 @@
+﻿using WebStoreGusev.Domain.Entities.Base.Interfaces;
+
+namespace WebStoreGusev.Models
+{
+    /// <summary>
+    /// Модель представления брендов товаров.
+    /// </summary>
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        /// <summary>
+        /// Количество товаров бренда.
+        /// </summary>
+        public int ProductsCount { get; set; }
+    }
+}
