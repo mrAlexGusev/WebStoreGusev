befbbdce680496357e136ed84c9cd5847139f44c
Александр Гусев <scorp.predateur@gmail.com>
2020-07-20 14:19:30 +0300

    Инициализация БД стартовыми данными

--- WebStoreGusev/Program.cs
+++ WebStoreGusev/Program.cs
@@ -1,11 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebStoreGusev.DAL;
 
 namespace WebStoreGusev
 {
@@ -13,14 +12,54 @@ namespace WebStoreGusev
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            #region Инициализация БД
+
+            var host = BuildWebHost(args);
+            
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    WebStoreContext context = services.GetRequiredService<WebStoreContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Oops. Something went wrong at DB initializing...");
+                }
+            }
+
+            host.Run();
+
+            #endregion
+
+            #region По умолчанию
+
+            //CreateHostBuilder(args).Build().Run();
+
+            #endregion
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        #region По умолчанию
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
+
+        #endregion
+
+        #region Инициализация БД
+
+        private static IWebHost BuildWebHost(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>()
+               .Build();
+
+        #endregion
     }
 }
