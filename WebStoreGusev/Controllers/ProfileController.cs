f893c5a11bd0cab59ec9106a4ec8253be7fffe05
Александр Гусев <scorp.predateur@gmail.com>
2020-08-12 14:23:16 +0300

    Личный кабинет пользователя с историей заказов

---
+++ WebStoreGusev/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStoreGusev.Infrastructure.Interfaces;
+using WebStoreGusev.Models;
+
+namespace WebStoreGusev.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IOrdersService ordersService;
+
+        public ProfileController(IOrdersService ordersService)
+        {
+            this.ordersService = ordersService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            var orders = ordersService.GetUserOrders(User.Identity.Name);
+            var orderModels = new List<UserOrderViewModel>(orders.Count());
+
+            foreach (var order in orders)
+            {
+                orderModels.Add(new UserOrderViewModel()
+                {
+                    Id = order.Id,
+                    Name = order.Name,
+                    Address = order.Address,
+                    Phone = order.Phone,
+                    TotalSum = order.OrderItems.Sum(o => o.Price * o.Quantity)
+                });
+            }
+
+            return View(orderModels);
+        }
+    }
+}
