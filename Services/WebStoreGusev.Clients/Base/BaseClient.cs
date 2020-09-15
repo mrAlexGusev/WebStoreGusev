49a7c2645eeb663e1fc0997aac7e3c931dc969af
Александр Гусев <scorp.predateur@gmail.com>
2020-09-15 15:08:38 +0300

    Базовый класс клиентов для WebAPI

---
+++ Services/WebStoreGusev.Clients/Base/BaseClient.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WebStoreGusev.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected readonly string serviceAddress;
+        protected readonly HttpClient client;
+
+        protected BaseClient(IConfiguration configuration, 
+            string ServiceAddress)
+        {
+            serviceAddress = ServiceAddress;
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(configuration["WebApiURL"]),
+                DefaultRequestHeaders =
+                {
+                    Accept =
+                    {
+                        new MediaTypeWithQualityHeaderValue("application/json")
+                    }
+                }
+            };
+        }
+    }
+}
