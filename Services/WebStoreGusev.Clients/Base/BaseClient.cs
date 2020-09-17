03e5fe0c24184f8c31edb8ffb5bd6ebbf23d5d62
Александр Гусев <scorp.predateur@gmail.com>
2020-09-17 16:47:19 +0300

    minor fxd

--- Services/WebStoreGusev.Clients/Base/BaseClient.cs
+++ Services/WebStoreGusev.Clients/Base/BaseClient.cs
@@ -5,18 +5,31 @@ using System.Net.Http.Headers;
 
 namespace WebStoreGusev.Clients.Base
 {
+    /// <summary>
+    /// Базовый клиент.
+    /// </summary>
     public abstract class BaseClient
     {
         protected readonly string serviceAddress;
+
+        /// <summary>
+        /// Http клиент для связи.
+        /// </summary>
         protected readonly HttpClient client;
 
         protected BaseClient(IConfiguration configuration, 
             string ServiceAddress)
         {
             serviceAddress = ServiceAddress;
+
+            // Создаем экземпляр клиента
             client = new HttpClient
             {
+                // Базовый адрес, на котором будут хостится сервисы
                 BaseAddress = new Uri(configuration["WebApiURL"]),
+
+                // Устанавливаем хедер, который будет говорить серверу,
+                // чтобы он отправлял данные в формате json.
                 DefaultRequestHeaders =
                 {
                     Accept =
