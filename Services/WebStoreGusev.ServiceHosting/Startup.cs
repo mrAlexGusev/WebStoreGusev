3af891bd3cb56554c84c51663f98dc91d65a9928
Александр Гусев <scorp.predateur@gmail.com>
2020-09-18 16:10:58 +0300

    Перенос сервисов в проект WebStoreGusev.ServiceHosting

--- Services/WebStoreGusev.ServiceHosting/Startup.cs
+++ Services/WebStoreGusev.ServiceHosting/Startup.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebStoreGusev.Clients.Values;
+using WebStoreGusev.DAL;
+using WebStoreGusev.Domain.Entities.Identity;
+using WebStoreGusev.Infrastructure.Interfaces;
+using WebStoreGusev.Infrastructure.Services.InCookies;
+using WebStoreGusev.Infrastructure.Services.InMemory;
+using WebStoreGusev.Infrastructure.Services.InSQL;
+using WebStoreGusev.Interfaces.Api;
 
 namespace WebStoreGusev.ServiceHosting
 {
@@ -23,6 +33,41 @@ namespace WebStoreGusev.ServiceHosting
 
         public void ConfigureServices(IServiceCollection services)
         {
+           
+
+            services.AddDbContext<WebStoreContext>(options => options
+               .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddIdentity<User, IdentityRole>()
+               .AddEntityFrameworkStores<WebStoreContext>()
+               .AddDefaultTokenProviders();
+
+            // необязательно
+            services.Configure<IdentityOptions>(options =>
+            {
+                // password settings
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+
+                // lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // user settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+
+            services.AddSingleton<IEmployeesServices, InMemoryEmployeeService>();
+            services.AddScoped<IProductService, SqlProductService>();
+            services.AddScoped<ICartService, CookieCartService>();
+            services.AddScoped<IOrdersService, SqlOrdersService>();
+
             services.AddControllers();
         }
 
