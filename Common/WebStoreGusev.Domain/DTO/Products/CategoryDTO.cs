60810543bcbb5d730f980cac5fd8a3bedf640954
Александр Гусев <scorp.predateur@gmail.com>
2020-10-12 17:08:34 +0300

    DTO-модели для процесса передачи данных в WebAPI каталога товаров

---
+++ Common/WebStoreGusev.Domain/DTO/Products/CategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using WebStoreGusev.Domain.Entities.Base.Interfaces;
+
+namespace WebStoreGusev.Domain.DTO.Products
+{
+    public class CategoryDTO : INamedEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ Common/WebStoreGusev.Domain/DTO/Products/ProductDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStoreGusev.Domain.Entities.Base.Interfaces;
+
+namespace WebStoreGusev.Domain.DTO.Products
+{
+    public class ProductDTO : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+       
+        public int Order { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public BrandDTO Brand { get; set; }
+
+        public CategoryDTO Category { get; set; }
+    }
+}
